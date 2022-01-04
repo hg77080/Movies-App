7400aa6fca1cfbfb6b8dc723a968748bcb9f2cae
Himanshu Gupta <hg77080@gmail.com>
2022-01-04 11:12:42 +0530

    first commit

---
+++ src/API/secrets.js
@@ -0,0 +1,3 @@
+export const API_URL = "https://api.themoviedb.org/3";
+export const API_KEY = "f7caf344f1bf22c647f6ce4768cff4ca";
+export const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
---
+++ src/Components/Favourite/Favourite.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Movies from '../Movies/Movies.jsx';
+// import "./Favourite.css";
+class Favourite extends Component {
+    state = {  }
+    render() { 
+        return (
+            <h1>Favourite Movie</h1>
+            );
+    }
+}
+ 
+export default Favourite;
